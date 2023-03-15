d67270e9c944b2adaa285a2f2793fe9ecb5888b1
Abhay Mutalikdesai <abhaymutalikdesai1@gmail.com>
2023-03-15 16:00:12 +0530

    first 6 tails added

---
+++ src/main/java/qa/netiq/pages/GroupManagementPage.java
@@ -0,0 +1,31 @@
+package pages;
+
+import com.microsoft.playwright.Page;
+
+public class GroupManagementPage {
+	private Page page;
+	private String pageLoc = "a[routerlink=\"grouppage\"]";
+	private String expectedUrl = "https://10.71.36.143:9000/identityconsole/#/grouppage";
+	private String headerLoc = "div[class=\"pageHeading wordWrap_heading\"] h2";
+	private String expectedHeader = "Groups";
+	
+
+	public GroupManagementPage(Page page) {
+		this.page = page;
+		page.click(HelperMethods.homeLoc);
+		page.click(pageLoc);
+		page.waitForURL(expectedUrl);
+	}
+
+	public GroupManagementPage headerVal() {
+		String headerTxt = HelperMethods.headerOpr(page, headerLoc);
+		HelperMethods.validation(headerTxt, expectedHeader, "header not matched");
+		return this;
+	}
+
+	public GroupManagementPage urlVal() {
+		String actualUrl = HelperMethods.urlOpr(page);
+		HelperMethods.validation(actualUrl, expectedUrl, "url not matched");
+		return this;
+	}
+}
